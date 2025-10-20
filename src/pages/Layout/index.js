69bb8108a413be3ae6766829cf4ba2d717a5098e
Junbiao06 <Junbiao06@gmail.com>
2025-10-20 22:28:53 +0800

    token注入

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,4 +1,11 @@
+
+import { request } from "@/utils"
+import { useEffect } from "react"
+
 const Layout = () => {
+  useEffect(() => {
+    request.get('/user/profile')
+  })
   return <div>this is layout</div>
 }
 
