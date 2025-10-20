4cb776ea27167978816234676dec95ba7a122deb
Junbiao06 <Junbiao06@gmail.com>
2025-10-20 22:55:05 +0800

    二级路由的配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 // 路由配置
 import Layout from "@/pages/Layout";
 import Login from "@/pages/Login";
+import Home from "@/pages/Home"
+import Article from "@/pages/Article"
+import Publish from "@/pages/Publish"
 
 import { createBrowserRouter } from "react-router-dom";
 import { AuthRoute } from "@/components/AuthRoute";
@@ -9,11 +12,21 @@ import { AuthRoute } from "@/components/AuthRoute";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <AuthRoute> <Layout /></AuthRoute>
-  },
-  {
-    path: "/layout",
-    element: <Layout />
+    element: <AuthRoute> <Layout /></AuthRoute>,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: 'article',
+        element: <Article />
+      },
+      {
+        path: 'publish',
+        element: <Publish />
+      }
+    ]
   },
   {
     path: "/login",
