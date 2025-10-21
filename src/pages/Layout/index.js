523c7e0fffbe66c37bf298381838f9d0bdcb50bf
Junbiao06 <Junbiao06@gmail.com>
2025-10-21 20:57:57 +0800

    退出登录的实现

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -10,7 +10,7 @@ import './index.scss'
 import { Outlet, useLocation, useNavigate } from 'react-router-dom'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { fetchuserInfo } from '@/store/modules/user'
+import { clearUserInfo, fetchuserInfo } from '@/store/modules/user'
 
 const { Header, Sider } = Layout
 
@@ -52,6 +52,10 @@ const GeekLayout = () => {
   }, [dispatch])
 
   const name = useSelector(state => state.user.userInfo.name)
+  const onConfirm = () => {
+    dispatch(clearUserInfo())
+    navigate('/login')
+  }
 
   return (
     <Layout>
@@ -60,7 +64,7 @@ const GeekLayout = () => {
         <div className="user-info">
           <span className="user-name">{name}</span>
           <span className="user-logout">
-            <Popconfirm title="是否确认退出？" okText="退出" cancelText="取消">
+            <Popconfirm title="是否确认退出？" okText="退出" cancelText="取消" onConfirm={onConfirm}>
               <LogoutOutlined /> 退出
             </Popconfirm>
           </span>
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,7 +1,7 @@
 // 和用户相关的状态管理
 
 import { createSlice } from "@reduxjs/toolkit";
-import { request } from "@/utils"
+import { removeToken, request } from "@/utils"
 import { setToken as _setToken, getToken } from "@/utils";
 
 const userStore = createSlice({
@@ -17,12 +17,17 @@ const userStore = createSlice({
     },
     setuserInfo(state, action) {
       state.userInfo = action.payload
+    },
+    clearUserInfo(state) {
+      state.token = ''
+      state.userInfo = {}
+      removeToken()
     }
   }
 })
 
 // 解构出actionCreator
-const { setToken, setuserInfo } = userStore.actions
+const { setToken, setuserInfo, clearUserInfo } = userStore.actions
 
 // 获取reduer函数
 const userReducer = userStore.reducer
@@ -45,6 +50,6 @@ const fetchuserInfo = () => {
   }
 }
 
-export { fetchLogin, fetchuserInfo, setToken }
+export { fetchLogin, fetchuserInfo, clearUserInfo }
 
 export default userReducer
